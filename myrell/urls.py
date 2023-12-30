7de660efa5dedeee11615046a186b7ed9d8e32e6
wildanmaulidy <sandoreswildan@gmail.com>
2023-12-30 18:12:24 +0700

    *

--- myrell/urls.py
+++ myrell/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home,tentang,kontak
+from .views import home,checkout,pesan,keranjang,login
 urlpatterns = [
     path('', home, name='home.html'),
-    path('tentang', tentang, name='tentang.html'),
-    path('kontak', kontak, name='kontak.html'),
+    path('checkout', checkout, name='checkout.html'),
+    path('pesan', pesan, name='pesan.html'),
+    path('keranjang', keranjang, name='keranjang.html'),
+    path('login', login, name='login.html')
 ]
--- myrell/views.py
+++ myrell/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from django.contrib.auth import authenticate,login
+
+
+
 
 def home(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
 
-def tentang(request):
-    return render(request, 'tentang.html')
+def checkout(request):
+    return render(request, 'checkout.html')
 
 
-def kontak(request):
-    return render(request, 'kontak.html')
+def pesan(request):
+    return render(request, 'pesan.html')
 
+def keranjang(request):
+    return render(request, 'keranjang.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('beranda')
+            else:
+                form.add_error(None, "Username atau password salah.")
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
